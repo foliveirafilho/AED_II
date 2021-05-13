75058f8ef69dccca74136352c9539bef09c59a6c
Francisco Oliveira <franciscofilho1109@gmail.com>
2021-05-13 19:57:29 -0400

    Implementação do split

--- codebench/arvores/arvoreb.c
+++ codebench/arvores/arvoreb.c
@@ -11,12 +11,36 @@ struct no{
 
 };
 
+No *criaNo();
+No* criaArvore();
 No* buscaChave(No *arvore, int chave);
+No* divide(No **pai, int indiceFilho);
+int ehFolha(No *no);
 
 int main(){
 
 
     return 0;
+
+}
+
+No *criaNo(){
+    No *no = malloc(sizeof *no);
+
+    no->qtdeChaves = 0;
+
+    for(int i = 0; i < (2 * ORDEM) - 1; i++)
+        no->chaves[i] = 0;
+
+    for(int i = 0; i < (2 * ORDEM); i++)
+        no->nos[i] = NULL;
+
+    return no;
+
+}
+
+No* criaArvore(){
+    return criaNo();
 
 }
 
@@ -39,5 +63,50 @@ No* buscaChave(No *arvore, int chave){
     }
 
     return NULL; // a arvore e NULL, nao ha busca
+
+}
+
+No* divide(No **pai, int indiceFilho){
+    No *no = criaNo();
+
+    no->qtdeChaves = ORDEM - 1;
+    (*pai)->nos[indiceFilho]->qtdeChaves = ORDEM - 1;
+
+    for(int i = 0; i < ORDEM - 1; i++)
+        no->chaves[i] = (*pai)->nos[indiceFilho]->chaves[i + ORDEM];
+
+    if(!ehFolha((*pai)->nos[indiceFilho])){
+        for(int i = 0; i < ORDEM; i++)
+            no->nos[i] = (*pai)->nos[indiceFilho]->nos[i + ORDEM];
+
+    }
+
+    for(int i = (*pai)->qtdeChaves + 1; i > indiceFilho + 1; i--)
+        (*pai)->nos[i + 1] = (*pai)->nos[i]; //shift nos filhos
+
+    (*pai)->nos[indiceFilho + 1] = no;
+
+    for(int i = (*pai)->qtdeChaves; i > indiceFilho; i--)
+        (*pai)->chaves[i] = (*pai)->chaves[i - 1]; ///shift nas chaves
+
+    (*pai)->chaves[indiceFilho] = (*pai)->nos[indiceFilho]->chaves[ORDEM - 1];
+    (*pai)->qtdeChaves++;
+
+    return *pai;
+
+}
+
+int ehFolha(No *no){
+    int i = 0;
+
+    while(i < no->qtdeChaves){
+        if(no->nos[i] != NULL)
+            return 0;
+
+        i++;
+
+    }
+
+    return 1;
 
 }
